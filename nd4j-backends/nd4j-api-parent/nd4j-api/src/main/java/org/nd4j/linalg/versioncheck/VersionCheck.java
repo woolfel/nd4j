219247f6d55257a674790b302fb5d8549156f388
Alex Black <blacka101@gmail.com>
2017-08-03 21:54:39 +1000

    Improve version check - check for pre-0.9.1 versions on classpath

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/versioncheck/VersionCheck.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/versioncheck/VersionCheck.java
@@ -40,6 +40,62 @@ public class VersionCheck {
             log.warn("ND4J FAILED VERSION CHECK - {} DIFFERENT VERSIONS FOUND", versions.size());
             logVersions();
         }
+
+        List<String> legacy = legacyVersionsPresent();
+        if( legacy.size() > 0){
+            log.warn("ND4J FAILED VERSION CHECK - PRE-0.9.1 VERSIONS FOUND");
+            for(String s : legacy){
+                log.info("Old version: {}", s);
+            }
+        }
+    }
+
+    private static List<String> legacyVersionsPresent(){
+
+        //Check for 0.9.0 or earlier of: DataVec, nd4j-native, nd4j-cuda, DL4J
+        //We know if the specified classes exist, and the version info class doesn't, it must be
+        // a 0.9.0 or earlier version
+        //This isn't the most elegant solution, but it should work
+
+
+        String datavecClass = "org.datavec.api.writable.DoubleWritable";    //Arbitrary class that should have been stable
+        String datavecVersionInfoClass = "org.datavec.api.versioncheck.DataVecApiVersionInfo";
+        boolean oldDatavecVersion = checkOldVersion(datavecClass, datavecVersionInfoClass);
+
+        String dl4jClass = "org.deeplearning4j.nn.conf.MultiLayerConfiguration";
+        String dl4jVersionInfoClass = "org.deeplearning4j.versioninfo.Dl4jNnVersionInfo";
+        boolean oldDl4jVersion = checkOldVersion(dl4jClass, datavecVersionInfoClass);
+
+        //TODO: Arbiter, RL4J
+
+        if(!oldDatavecVersion && !oldDl4jVersion){
+            return Collections.emptyList();
+        }
+        List<String> ret = new ArrayList<>(2);
+        if(oldDatavecVersion){
+            ret.add("org.datavec : datavec-api : (unknown pre-0.9.1 version)");
+        }
+        if(oldDl4jVersion){
+            ret.add("org.deeplearning4j : deeplearning4j-nn : (unknown pre-0.9.1 version)");
+        }
+
+        return ret;
+    }
+
+    private static boolean checkOldVersion(String someClass, String versionInfoClass ){
+        try{
+            Class.forName(someClass);
+            //No exception: does exist -> dependency is on classpath
+            try{
+                Class.forName(versionInfoClass);
+            } catch (ClassNotFoundException e){
+                //Version info class not found -> dependency IS on classpath, but it's a 0.9.0 or earlier version
+                return true;
+            }
+        } catch (ClassNotFoundException e){
+            //Not found -> not on classpath
+        }
+        return false;
     }
 
     public static void logVersions(){
