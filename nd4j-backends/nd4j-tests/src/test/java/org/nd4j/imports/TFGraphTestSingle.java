c68b079fcd433b8b24929752f9861041da8c0dd5
eraly <susan.eraly@gmail.com>
2017-11-13 18:21:34 -0800

    comments for single test

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestSingle.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestSingle.java
@@ -12,14 +12,21 @@ import static org.nd4j.imports.TFGraphTestAll.outputVars;
 import static org.nd4j.imports.TFGraphTestAll.testSingle;
 
 /**
- * Created by susaneraly on 11/12/17.
+ * TFGraphTestAll will run all the checked in TF graphs and
+ *      compare outputs in nd4j to those generated and checked in from TF.
+ *
+ * This file is to run a single graph that is checked in to aid in debug.
+ * Simply change the modelName String in testOne() to correspond to the directory name the graph lives in
+ *  - eg. to run the graph for 'bias_add' i.e checked in under tf_graphs/examples/bias_add
+ *  set modelName to "bias_add"
+ *
  */
 @Slf4j
 public class TFGraphTestSingle {
 
     @Test
     public void testOne() throws  Exception {
-        //String modelName = "transform_0";
+        //String modelName = "bias_add";
         String modelName = "conv_0";
         String modelDir = new ClassPathResource("tf_graphs/examples/" + modelName).getFile().getAbsolutePath();
         Map<String, INDArray> inputs = inputVars(modelDir);
